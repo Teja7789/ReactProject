7416f8b42b44980727785e84b94f00dc9174fb70
Teja7789 <rx100789@gmail.com>
2024-01-09 04:28:09 +0530

    (scrimba_1)

---
+++ src/Assignment/Scrimba/ScrimbaPractice/Day1/FirstDay.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function FirstDay() {
+  return (
+    <div>
+        <h1>First Day of the Week</h1>
+        <ul>
+            <li>React is Easy</li>
+            <li>React is awesome</li>
+            <li>React is must needed skill</li>
+            <li>React is dev fav</li>
+            </ul>
+    </div>
+  )
+}
+
+export default FirstDay
